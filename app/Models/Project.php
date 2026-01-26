3a11d2007ff816c2c0c336760da8b94b39f2d668
kwon-soonwoo <soonwookwon06@gmail.com>
2026-01-26 16:21:48 -0500

    refactor(project): update project retrieval and sorting logic

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -66,4 +66,64 @@ class Project extends Model
     {
         return round($this->start_time->diffInMinutes($this->end_time) / 60, 1);
     }
+
+    public function getHoursForUser($netid)
+    {
+        $userShifts = $this->shifts()->where('netid', $netid)->get();
+        
+        $totalHours = 0;
+        $billedHours = 0;
+        $unbilledHours = 0;
+        
+        foreach ($userShifts as $shift) {
+            $hours = $shift->duration ? $shift->duration / 60 : 0;
+            $totalHours += $hours;
+            
+            if ($shift->billed) {
+                $billedHours += $hours;
+            } else {
+                $unbilledHours += $hours;
+            }
+        }
+        
+        return [
+            'total_hours' => round($totalHours, 2),
+            'billed_hours' => round($billedHours, 2),
+            'unbilled_hours' => round($unbilledHours, 2)
+        ];
+    }
+
+    public function getAllHours()
+    {
+        $allShifts = $this->shifts()->get();
+        
+        $totalHours = 0;
+        $billedHours = 0;
+        $unbilledHours = 0;
+        
+        foreach ($allShifts as $shift) {
+            $hours = $shift->duration ? $shift->duration / 60 : 0;
+            $totalHours += $hours;
+            
+            if ($shift->billed) {
+                $billedHours += $hours;
+            } else {
+                $unbilledHours += $hours;
+            }
+        }
+        
+        return [
+            'total_hours' => round($totalHours, 2),
+            'billed_hours' => round($billedHours, 2),
+            'unbilled_hours' => round($unbilledHours, 2)
+        ];
+    }
+
+    public function scopeAssignedToUser($query, $netid)
+    {
+        return $query->join('project_user', 'projects.id', '=', 'project_user.project_id')
+            ->where('project_user.user_netid', $netid)
+            ->select('projects.*')
+            ->distinct();
+    }
 }
